2273665732893ba05ac70ef0be8ee1ef63809ce9
Tony Jiang <52139961+1tonyjiang@users.noreply.github.com>
2021-06-10 23:17:03 -0700

    outputs results to csv

--- src/sentiment_apis/backtestSentiment.py
+++ src/sentiment_apis/backtestSentiment.py
@@ -3,6 +3,9 @@ from sentiment_apis.sentimentAnalyzer import NLTKsentimentAnalyzer
 from sentiment_apis.twintSearch import twintSearcher
 import csv
 
+TIME_INTERVAL_DELTA = datetime.timedelta(minutes=1)
+SENTIMENT_RESET = 360  # reset sentiment every 6 hours
+
 
 def backtester(term: str, start: datetime, end: datetime):
     file = open("output/SentimentTrade" + term + ".csv", "w")
@@ -12,13 +15,11 @@ def backtester(term: str, start: datetime, end: datetime):
     searcher = twintSearcher()
     tweets = searcher.get_tweets_term(term, start, end)
     analyzer = NLTKsentimentAnalyzer(tweets)
-    delta = datetime.timedelta(hours=1)
 
     while start < end:
-        next = start + delta
+        next = start + TIME_INTERVAL_DELTA
         writer.writerow([start, next, analyzer.evaluate(start, next)])
-
         start = next
-        next += delta
+        next += TIME_INTERVAL_DELTA
 
     file.close()
